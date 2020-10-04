41912ae79fc96e174d8b6313602a6f2394de2c2f
phantienthinh <37545642+phantienthinh@users.noreply.github.com>
2020-10-04 12:19:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/waveview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.waveview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        pauseBt.setOnClickListener {
+            waveView.pauseAnimation()
+        }
+        resumeBt.setOnClickListener {
+            waveView.resumeAnimation()
+        }
+    }
+}
